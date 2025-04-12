12d4b47948b16be576d8db2f68d61cd6964bbbd7
tynguyen2k1 <nguyenvantybg01@gmail.com>
2025-04-12 17:36:02 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('groups.index');
 });
 
 Route::get('/groups', [GroupController::class, 'index'])->name('groups.index');
